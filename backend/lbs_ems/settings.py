8aea42344484216161fd69f0057ecbf35dd33a13
Vasudev <vasudevbinu25@gmail.com>
2025-11-09 05:53:34 +0530

    settings issue

--- backend/lbs_ems/settings.py
+++ backend/lbs_ems/settings.py
@@ -108,9 +108,12 @@ WSGI_APPLICATION = 'lbs_ems.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default='your_render_postgres_url')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
